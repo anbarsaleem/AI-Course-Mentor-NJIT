289d900799a7922c02b8a4930a16bb285e61485a
Anbar Saleem <45743448+anbarsaleem@users.noreply.github.com>
2024-08-23 00:44:02 -0400

    moved assistant resource allocation to backend and optimized with multithreading

--- backend/backend_runner.py
+++ backend/backend_runner.py
@@ -1,12 +1,16 @@
 from fetch_and_parse_php_to_dataframe import fetch_and_parse_php
-from njit_catalog_scraper import scrape_courses
+from njit_catalog_scraper import njit_catalog_scraper
+from assistant_resource_allocate import assistant_resource_allocate
 
 def run_all_backends():
     # Fetch and upload NJIT PHP data to Digital Ocean Spaces
     fetch_and_parse_php()
 
     # Scrape and upload NJIT Course Data to Digital Ocean Spaces
-    scrape_courses()
+    njit_catalog_scraper()
+
+    # Allocate resources to the Assistant
+    assistant_resource_allocate()
 
 if __name__ == "__main__":
     run_all_backends()
--- backend/njit_catalog_scraper.py
+++ backend/njit_catalog_scraper.py
@@ -22,14 +22,13 @@ load_dotenv()
 DO_SPACES_KEY = os.getenv('DO_SPACES_KEY')
 DO_SPACES_SECRET = os.getenv('DO_SPACES_SECRET')
 DO_SPACES_REGION = os.getenv('DO_SPACES_REGION', 'nyc3')
-DO_SPACES_ENDPOINT = os.getenv('DO_SPACES_ENDPOINT', 'https://nyc3.digitaloceanspaces.com')
 DO_SPACES_BUCKET = os.getenv('DO_SPACES_BUCKET')
 
 # Configure the boto3 client
 session = boto3.session.Session()
 client = session.client('s3',
                         region_name=DO_SPACES_REGION,
-                        endpoint_url=DO_SPACES_ENDPOINT,
+                        endpoint_url='https://nyc3.digitaloceanspaces.com',
                         aws_access_key_id=DO_SPACES_KEY,
                         aws_secret_access_key=DO_SPACES_SECRET)
 
@@ -254,7 +253,7 @@ def scrape_link(url):
         logger.error(f"Failed to scrape main page {url}: {e}")
 
 # Main function to scrape courses
-def main():
+def njit_catalog_scraper():
     logger.info("Starting course scraping process")
     try:
         # Read URLs to scrape from file
@@ -278,4 +277,4 @@ def main():
 
 # To ensure compatibility with the backend runner
 if __name__ == "__main__":
-    main()
+    njit_catalog_scraper()
